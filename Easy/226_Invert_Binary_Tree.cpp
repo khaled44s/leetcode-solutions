5742d9b0e5225331b20f283c756a0e0b9f8b775b
Khaled <khaledtry2@gmail.com>
2026-06-09 23:52:23 +0600

    Solve Leetcode #226 Invert Binary Tree

---
+++ Easy/226_Invert_Binary_Tree.cpp
@@ -0,0 +1,18 @@
+/*
+TC: O(n)
+SC: O(h)
+*/
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr) {
+            return nullptr;
+        }
+
+        swap(root->left, root->right);
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
+    }
+};
