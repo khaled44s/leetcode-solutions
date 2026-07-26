b67051aa5ca89277489bec0eec88e532505c34e9
Khaled <khaledtry2@gmail.com>
2026-07-26 23:21:17 +0600

    Solve Leetcode #1051 Height Checker

---
+++ Easy/1051_Height_Checker.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int>sortedHeights = heights;
+        sort(sortedHeights.begin(), sortedHeights.end());
+        int ans = 0;
+        for (int i = 0; i < heights.size(); i++) {
+            if (heights[i] != sortedHeights[i]) {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+};
