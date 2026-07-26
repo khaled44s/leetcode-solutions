1ebb0fe2eb7fe9dfa36ca91bcee4c58fa94d3721
Khaled <khaledtry2@gmail.com>
2026-07-26 23:41:52 +0600

    Solve Leetcode #933 Number of Recent Calls

---
+++ Easy/933_Number_of_Recent_Calls.cpp
@@ -0,0 +1,15 @@
+class RecentCounter {
+private:
+    vector<int> records;
+    int start;
+public:
+    RecentCounter() {
+        start = 0;
+    }
+    
+    int ping(int t) {
+        records.push_back(t);
+        while (records[start] < (t-3000)) start++;
+        return records.size() - start;
+    }
+};
