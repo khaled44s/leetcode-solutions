9c5c543a9b62f0390ca155bb22cb0397b9fca082
Khaled <khaledtry2@gmail.com>
2026-07-27 23:14:46 +0600

    Solve Leetcode #908 Smallest Range I

---
+++ Easy/908_Smallest_Range_I.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int smallestRangeI(vector<int>& nums, int k) {
+        int mx = nums[0], mn = nums[0];
+        for (int a : nums) mx = max(mx, a), mn = min(mn, a);
+        return max(0, mx - mn - 2 * k);
+    }
+};
